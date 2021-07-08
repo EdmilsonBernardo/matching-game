cf0d0b6eb16afdd38a64319228d536e864bdf0f7
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-07-08 10:49:52 -0300

    Starting the project

---
+++ main.js
@@ -0,0 +1,9 @@
+import CardGame from './src/components/CardGame';
+import './src/styles/settings/colors.css';
+import './src/styles/generic/generic.css';
+import './src/styles/elements/base.css';
+
+const $root = document.querySelector("#root");
+const $htmlCardGame = CardGame();
+
+$root.insertAdjacentHTML('beforeend', $htmlCardGame);
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function CardGame() {
+  return `
+    <article class="card-game">
+      <img class="img-game" src="images/questionando.png" alt="Logo traseiro">
+    </article>
+  `
+}
+
+export default CardGame;
